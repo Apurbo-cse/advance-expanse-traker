c3b3170d301709496d732b541206d678edc74c1d
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-14 17:17:34 +0600

    Create Transaction.tsx

---
+++ src/components/Transaction.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const Transaction: React.FC<Props> = ({ children }) => {
+    return (
+        <div className="App">
+
+            <div className="header">
+                <h1>Expense Tracker</h1>
+            </div>
+
+            <div className="main">
+                {children}
+            </div>
+
+            <div className="footer">&copy;2022 Learn with Sumit</div>
+        </div>
+    )
+}
+
+export default Transaction;
